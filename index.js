75d73f5e2b7007f8cd930745120c33b46c75e3cb
vuong0809 <nguyentuanvuongfacebook@gmail.com>
2022-12-05 01:36:33 +0000

    a

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-// const fs = require("fs");
-// const { parse } = require("csv-parse");
-
 
 var token = [
     {
+        token: "cR4V5tARMrPVJtuH00a8",
+        device_type: "GPS",
+        name: "/4/1"
+    }, {
+        token: "PeYW8mFvsOLs16SqVytu",
+        device_type: "DOOR",
+        name: "/2/1/2"
+    }, {
+        token: "prASFQFmkQHTFoMFsJJ2",
+        device_type: "DOOR",
+        name: "/2/1/5"
+    }, {
+        token: "MASkRqgEYnU8DDOuRQ0B",
+        device_type: "DOOR",
+        name: "/2/1/6"
+    }, {
         token: "xSlRL32JSZgzqdm4VFxQ",
-        device_type: "WEATHER"
+        device_type: "WEATHER",
+        name: "/2/1/1"
+    }, {
+        token: "QWiP55rvirtCiPhAJy7n",
+        device_type: "WEATHER",
+        name: "/2/1/3"
     },
     {
-        token: "QWiP55rvirtCiPhAJy7n",
-        device_type: "WEATHER"
+        token: "bAfZaP7Bal4oqcWIHKD5",
+        device_type: "WEATHER",
+        name: "/2/1/4"
+    },
+    {
+        token: "IP6JEf9Bu4BaFzS9Hwcc",
+        device_type: "WEATHER",
+        name: "/3/1"
+    },
+    {
+        token: "0nWwUz3IJB60V3g7vxeW",
+        device_type: "WEATHER",
+        name: "/3/2"
     }
 ]
 var count = 0
--- service.js
+++ service.js
@@ -2,8 +2,9 @@ const { workerData, parentPort } = require('worker_threads')
 
 parentPort.postMessage(workerData)
 
-var token = workerData.device.token
-var mqtt = require('mqtt')
+const token = workerData.device.token
+const device_type = workerData.device.device_type
+const mqtt = require('mqtt')
 const client = mqtt.connect('mqtt://localhost:1883', {
     username: token
 })
@@ -13,27 +14,69 @@ var time_old = 0
 var time_now = 0
 var time_set = 60000
 
-var max = 11
+var max = 15
 var min = 10
-var decimals = 2
+var decimals = 5
 
 client.on('connect', function () {
-    console.log('connected', workerData.index)
+    console.log('connected', workerData.index, ' device ', workerData.device.name)
     setInterval(() => {
-        data = {
-            "temp": (Math.random() * (max - min) + min).toFixed(decimals || 2),
-            "hum": (Math.random() * (50 - 45) + 45).toFixed(decimals || 2)
-        }
-        client.publish('v1/devices/me/attributes', JSON.stringify(data));
-
-        if (time_now - time_old >= time_set) {
-            client.publish('v1/devices/me/telemetry', JSON.stringify(data));
-
-            time_old = time_now
+        if (device_type == 'WEATHER') {
+            send_WEATHER()
+        } else if (device_type == 'DOOR') {
+            send_DOOR()
+        } else if (device_type == 'GPS') {
+            send_GPS()
         }
         time_now += 1000
     }, 1000)
 })
 
 
+function send_WEATHER() {
+    data = {
+        "temp": (Math.random() * (max - min) + min).toFixed(decimals || 2),
+        "hum": (Math.random() * (50 - 45) + 45).toFixed(decimals || 2)
+    }
+    client.publish('v1/devices/me/attributes', JSON.stringify(data));
+    if (time_now - time_old >= time_set) {
+        // client.publish('v1/devices/me/telemetry', JSON.stringify(data));
+        time_old = time_now
+    }
+}
+
+
+function send_GPS() {
+    data = {
+        "location": {
+            "lat": 0,
+            "lng": 0,
+            "speed": 0
+        }
+    }
+    client.publish('v1/devices/me/attributes', JSON.stringify(data));
+    if (time_now - time_old >= time_set) {
+        // client.publish('v1/devices/me/telemetry', JSON.stringify(data));
+        time_old = time_now
+    }
+}
+
+
+var max_door = 30000
+var min_door = 60000 * 15
+var time_door_set = Math.floor(Math.random() * (max_door - min_door)) + min_door;
+var door_status = false
+function send_DOOR() {
+    if (time_now - time_old >= time_door_set) {
+        time_old = time_now
+        time_door_set = Math.floor(Math.random() * (max_door - min_door)) + min_door;
+        door_status = !door_status
+        // console.log(`device name ${workerData.device.name} time set ${time_door_set} status ${door_status}`)
+        var data = {
+            "door_status": door_status
+        }
+        client.publish('v1/devices/me/attributes', JSON.stringify(data));
+        // client.publish('v1/devices/me/telemetry', JSON.stringify(data));
+    }
+}
 
